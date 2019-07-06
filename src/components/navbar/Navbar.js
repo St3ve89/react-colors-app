eaa99b380c2c646a7206afa109d2ea00ea2ff2c1
Istvan Acs <istvan.acs89@gmail.com>
2019-07-06 18:40:57 +0100

    Add snackbar and style select

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
@@ -10,14 +13,20 @@ class Navbar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      format: 'hex'
+      format: 'hex',
+      open: false
     }
-    this.handleChange = this.handleChange.bind(this);
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackBar = this.closeSnackBar.bind(this);
   }
 
-  handleChange(ev) {
-    this.setState({format: ev.target.value});
+  handleFormatChange(ev) {
+    this.setState({format: ev.target.value, open: true});
     this.props.handleChange(ev.target.value)
+  }
+
+  closeSnackBar() {
+    this.setState({open: false});
   }
 
   render() {
@@ -35,12 +44,27 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select value={format} onChange={this.handleChange}>
+          <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format Changed to {format.toUpperCase()}!</span>}
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          onClose={this.closeSnackBar}
+          action={[
+            <IconButton onClick={this.closeSnackBar} color="inherit" key="close" aria-label="close">
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     )
   }
