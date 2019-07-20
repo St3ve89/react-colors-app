d56b646893b93839cc5c89bacc2414e8ee633460
Istvan Acs <istvan.acs89@gmail.com>
2019-07-20 18:25:49 +0100

    Add link to palettes

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import seedColors from './helper/seedColors';
 import { generatePalette } from './helper/colorHelpers';
 
 class App extends Component {
-
   findPalette(id) {
     return seedColors.find(function(palette) {
-      return palette.id === id
-    })
+      return palette.id === id;
+    });
   }
 
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <PaletteList palettes={seedColors}/>} />
+        <Route
+          exact
+          path="/"
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
         <Route
           exact
           path="/palette/:id"
-          render={routeProps => ( 
+          render={routeProps => (
             <Palette
-              palette={generatePalette(this.findPalette(routeProps.match.params.id))}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
             />
-          )} 
+          )}
         />
       </Switch>
       // <div>
--- src/components/paletteList/PaletteList.js
+++ src/components/paletteList/PaletteList.js
@@ -5,36 +5,39 @@ import { Link } from 'react-router-dom';
 
 const styles = {
   root: {
-    backgroundColor: "blue",
-    height: "100vh",
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "center"
-
+    backgroundColor: 'blue',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
   },
   container: {
-    width:"50%",
-    desplay: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    flexWrap: "wrap"
+    width: '50%',
+    desplay: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap'
   },
   nav: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    color: "white"
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    color: 'white'
   },
   palettes: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%'
   }
-}
+};
 
 class PaletteList extends Component {
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
+
   render() {
     const { classes, palettes } = this.props;
 
@@ -42,16 +45,19 @@ class PaletteList extends Component {
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1>React colors</h1>
+            <h1> React colors </h1>
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <MiniPalette {...palette} />
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+              />
             ))}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
