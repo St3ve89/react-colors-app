b4b5ecb4cd287249144ede8aaf0c7f0497eee959
Istvan Acs <istvan.acs89@gmail.com>
2019-09-06 21:31:25 +0100

    Tweak form styles

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -16,6 +16,9 @@ const styles = theme => ({
     alignItems: 'center',
     height: '64px'
   },
+  hide: {
+    display: 'none'
+  },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
