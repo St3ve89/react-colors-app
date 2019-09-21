de2ee3b0fac432e8b239f677eade35dbf2e3b854
St3ve89 <istvan.acs89@gmail.com>
2019-09-21 09:32:07 +0100

    Make colorbox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,11 +15,11 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: props => (props.showingFullPalette ? '20%' : '50%')
+      height: props => (props.showingFullPalette ? '20%' : '33.333%')
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: props => (props.showingFullPalette ? '10%' : '50%')
+      height: props => (props.showingFullPalette ? '10%' : '20%')
     },
     [sizes.down('xs')]: {
       width: '100%',
@@ -118,7 +118,10 @@ export default {
       textAlign: 'center',
       marginBottom: '0',
       padding: '1rem',
-      textTransform: 'uppercase'
+      textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '5rem'
+      }
     },
     '& p': {
       fontSize: '2rem',
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
   palette: {
     height: '100vh',
@@ -34,6 +36,18 @@ export default {
       textTransform: 'uppercase',
       border: 'none',
       textDecoration: 'none'
+    },
+    [sizes.down('lg')]: {
+      width: '25%',
+      height: '33.333%'
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: '20%'
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: '10%'
     }
   }
 };
